073612322654bed690595825c3fb81787388040c
Jake Tran <jake.tranehs@gmail.com>
2019-08-13 13:02:52 -0700

    first commit

---
+++ src/README.md
@@ -0,0 +1,5 @@
+## New York Times API wrapper
+
+https://developer.nytimes.com/apis
+
+The New York Times developer API have many services and each have their own unique endpoints, filtering method as well as reponse protocols. This wrapper purpose is to congregate many of the services together and make the request methods as simple as possible without knowing all the unique endpoints and their attributes.
---
+++ src/index.js
@@ -0,0 +1,66 @@
+const request = require('request')
+
+// const baseUri = "http://api.nytimes.com/svc/mostpopular/v2"
+const baseUri = "http://api.nytimes.com/svc/"
+
+class TheTimes {
+  constructor(apiKey, query, filter) {
+    this.apiKey = apiKey
+    this.query = query
+    this.filter = filter
+  }
+
+  mostEmailed(callback) {
+    this._sendRequest("mostpopular/v2/mostemailed/all-sections/7?", callback)
+  }
+
+  mostViewed(callback) {
+    this._sendRequest("mostpopular/v2/mostviewed/all-sections/7?", callback)
+  }
+
+  mostShared(callback) {
+    this._sendRequest("mostpopular/v2/mostshared/all-sections/7?", callback)
+  }
+
+  topstories(callback) {
+      const sectionQuery = this.query ? this.query : 'home'
+      this._sendRequest(`topstories/v2/${sectionQuery}.json?`, callback)
+  }
+
+  search(callback) {
+      const existFilter = ''
+      if(this.filter !== null || this.filter !== undefined) {
+          existFilter == `&fq=${this.filter}`
+      }
+      this._sendRequest(`search/v2/articlesearch.json?q=${this.query}${existFilter}&`, callback)
+  }
+
+  archive(callback) {
+    //   /{year}/{month}.json
+      this._sendRequest(`archive/v1/${this.query}.json?`)
+  }
+
+  _sendRequest(service, callback) {
+    const url = `${baseUri}/${service}api-key=${this.apiKey}`
+
+    request(url, function(error, response, body) {
+      if (!error & response.statusCode === 200) {
+        if(JSON.parse(body).results){
+            callback(JSON.parse(body).results)
+        } else if (JSON.parse(body).response) {
+            callback(JSON.parse(body).response)
+        }
+      }
+    })
+  }
+}
+
+const times = new TheTimes("AlXgoIpRe2n0WiErV633AGifrdaBrHbH")
+
+times.topstories(function(data) {
+  console.log(data)
+})
+
+// request(`https://api.nytimes.com/svc/topstories/v2/science.json?api-key=AlXgoIpRe2n0WiErV633AGifrdaBrHbH`, function(error, response, body) {
+//     console.log(JSON.parse(body).response)
+// })
